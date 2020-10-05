1a22e4efe0f8425647d277c294d668e216d67c7e
soleilyasmina <mounlasolomon@gmail.com>
2020-10-05 18:17:55 -0400

    update readme and css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import Navbar from './Navbar';
-import Station from './Station';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import Station from "./Station";
+import { Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Station stationId="82bd"/>
-      <Station stationId="84ac"/>
-      <Station stationId="d2c6"/>
-      <Station stationId="3cf6"/>
+      <Route path="/union-square">
+        <Station stationId="82bd" />
+      </Route>
+      <Route path="/times-square">
+        <Station stationId="84ac" />
+      </Route>
+      <Route path="/atlantic-avenue">
+        <Station stationId="d2c6" />
+      </Route>
+      <Route path="/fulton-street">
+        <Station stationId="3cf6" />
+      </Route>
     </div>
   );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <header>
       <h1>Where's The Frickin Train</h1>
       <nav>
+        <Link to="/">Home</Link>
+        <Link to="/union-square">Union Square</Link>
+        <Link to="/times-square">Times Square</Link>
+        <Link to="/atlantic-avenue">Atlantic Avenue</Link>
+        <Link to="/fulton-street">Fulton Street</Link>
       </nav>
     </header>
   )
